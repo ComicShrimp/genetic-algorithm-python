40ac6e8434bcf27cd1b86903e4cd6d18e2853299
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2021-08-07 15:59:15 -0300

    feat: add fitness function

--- genetic-algorithm-python/main.py
+++ genetic-algorithm-python/main.py
@@ -12,6 +12,7 @@ class GenecticAlgorithm:
     products: List[Product]
     generation_limit: int
     fitness_limit: int
+    volume_limit: int
 
     def __init__(
         self,
@@ -19,6 +20,7 @@ class GenecticAlgorithm:
         products: List[Product],
         generation_limit: int,
         fitness_limit: int,
+        volume_limit: int,
     ) -> None:
         self.products = products
         self.population = self.__generate_population(
@@ -26,9 +28,24 @@ class GenecticAlgorithm:
         )
         self.generation_limit = generation_limit
         self.fitness_limit = fitness_limit
+        self.volume_limit = volume_limit
 
     def __generate_genome(self, length: int) -> Genome:
         return choices([0, 1], k=length)
 
     def __generate_population(self, size: int, genome_length: int) -> Population:
         return [self.__generate_genome(genome_length) for _ in range(size)]
+
+    def __fitness_function(self, genome: Genome) -> int:
+        volume = 0
+        value = 0
+
+        for i, product in enumerate(self.products):
+            if genome[i] == 1:
+                volume += product.volume
+                value += product.value
+
+                if volume > self.volume_limit:
+                    return 0
+
+        return value
